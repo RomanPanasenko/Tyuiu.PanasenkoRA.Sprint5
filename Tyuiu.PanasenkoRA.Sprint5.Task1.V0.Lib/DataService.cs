8e94a025e6f1eeebe68bde816cce36258448cde6
Tyuiu.PanasenkoRA@outlook.com <Tyuiu.PanasenkoRA@outlook.com>
2023-11-20 18:39:46 +0500

    Task1

---
+++ Tyuiu.PanasenkoRA.Sprint5.Task1.V0.Lib/DataService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+
+namespace Tyuiu.PanasenkoRA.Sprint5.Task1.V0.Lib
+{
+    public class DataService : ISprint5Task1V5
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask1.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            if (fileExists)
+            {
+                File.Delete(path);
+            }
+
+            double y;
+            string strY;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = Math.Round((5 - 3 * x + (1 + Math.Sin(x)) / (2 * x - 0.5)), 2);
+                strY = Convert.ToString(y);
+                if (x != stopValue)
+                {
+                    File.AppendAllText(path, strY + Environment.NewLine);
+                }
+                else
+                {
+                    File.AppendAllText(path, strY);
+                }
+            }
+            return path;
+
+        }
+    }
+}
---
+++ Tyuiu.PanasenkoRA.Sprint5.Task1.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.PanasenkoRA.Sprint5.Task1.V0.Lib;
+
+namespace Tyuiu.PanasenkoRA.Sprint5.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
